b5d0e86e4608b1b6963f42bc97e0a9afb9c4bfb1
Masatoshi Itoh <masatoshi9953@gmail.com>
2014-07-30 07:43:22 +0900

    initial commit

---
+++ unity_mqlib/MqSubscriber.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Threading;
+using System.Text;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.MessagePatterns;
+using RabbitMQ.Util;
+
+public class MqSubscriber : MonoBehaviour {
+    public string serverip;
+    public string exchange;
+    
+    ConnectionFactory cf;
+    IConnection conn;
+    IModel ch = null;
+    QueueingBasicConsumer consumer;
+
+    System.Collections.Queue queue = null;
+    string lastMessage;
+
+	// Use this for initialization
+	void Start () {
+        cf = new ConnectionFactory();
+        cf.HostName = serverip;
+        conn = cf.CreateConnection();
+
+        conn.ConnectionShutdown += new ConnectionShutdownEventHandler(LogConnClose);
+
+        ch = conn.CreateModel();
+        ch.ExchangeDeclare(exchange, "fanout");
+        string queueName = ch.QueueDeclare();
+
+        ch.QueueBind(queueName, exchange, "");
+        consumer = new QueueingBasicConsumer(ch);
+        ch.BasicConsume(queueName, true, consumer);
+
+        queue = new System.Collections.Queue();
+        queue.Clear();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ch == null) return;
+        BasicDeliverEventArgs ea;
+        while ((ea = (BasicDeliverEventArgs)consumer.Queue.DequeueNoWait(null)) != null)
+        {
+            var body = ea.Body;
+            var message = Encoding.UTF8.GetString(body);
+            queue.Enqueue(message);
+        }
+	}
+    
+    public static void LogConnClose(IConnection conn, ShutdownEventArgs reason)
+    {
+    }
+}
